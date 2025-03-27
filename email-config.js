303e61356041f8c821d03bd0f1c5d78dc4615fa4
anuroopbs <anuroopquestion7@gmail.com>
2025-03-27 02:21:09 +0000

    Add files via upload

--- email-config.js
+++ email-config.js
@@ -1,4 +1,11 @@
+
 (function() {
   // Initialize EmailJS with your public key
   emailjs.init("ZgXmlUv7-hSz3exxl");
+  
+  // Set default email template parameters
+  window.emailjsParams = {
+    from_email: "anuroopquestion7@gmail.com",
+    reply_to: "anuroopquestion7@gmail.com"
+  };
 })();
